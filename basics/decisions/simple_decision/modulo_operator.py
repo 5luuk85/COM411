d40ac6d055811a7f23b3925490436b1071cf3d95
repl.it user <replituser@example.com>
2020-10-06 11:40:26 +0000

    Added code to demonstrate the use of a modulus operator.

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,12 @@
+#Using the modulus operator % to determine if a number is odd or even
+
+print("Please ender a whole number.")
+num = int(input())
+remainder = num % 2
+#If a number is divided by 2 and the remainder is 0, then the number is an even number.
+#If there are any remainders then the number is odd
+
+if remainder ==0:
+  print(num,"is an even number!")
+else:
+  print(num,"is an odd number!")
