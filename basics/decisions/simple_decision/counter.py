4f478d65bd487f6e919b90346925d7242cf31fa8
repl.it user <replituser@example.com>
2020-10-08 16:31:14 +0000

    Added code to demonstrate the use of a nested decision.

--- basics/decisions/simple_decision/counter.py
+++ basics/decisions/simple_decision/counter.py
@@ -5,33 +5,34 @@ print()
 
 print("Please enter the first whole number.")
 first_no = int(input())
-remainder_first = first_no % 2
 print()
 
 print("Please enter the second number.")
 second_no = int(input())
-remainder_second = second_no % 2
 print()
 
 print("Please enter the third number.")
 third_no = int(input())
-remainder_third = third_no % 2
 print()
 
+#The variables with a base value of 0
 even_numbers = 0
 odd_numbers = 0
 
-if (remainder_first ==0):
-  even_numbers = +1
-else: odd_numbers = +1
+#The if statements will take the integer inputs above do the calculations needed. If input is divided by 2 and the remainder is 0 then a value of 1 will be added to the even_numbers variable.
+if (first_no % 2 ==0):
+  even_numbers = even_numbers + 1
+else: odd_numbers = odd_numbers + 1
 
-if (remainder_second ==0):
-  even_numbers = +1
-else: odd_numbers = +1
+#Note the odd_numbers = odd_numbers + 1, odd_numbers has to equals it self and then plus one to its saved value.
 
-if (remainder_third ==0):
-  even_numbers = +1
-else: odd_numbers = +1
+if (second_no % 2 ==0):
+  even_numbers = even_numbers + 1
+else: odd_numbers = odd_numbers + 1
+
+if (third_no % 2 ==0):
+  even_numbers = even_numbers + 1
+else: odd_numbers = odd_numbers + 1
 
 
 print("There were {} even numbers".format (even_numbers),"and","{} odd numbers.".format(odd_numbers))
---
+++ basics/decisions/simple_decision/nested.py
@@ -0,0 +1,17 @@
+#Nested  decisions. Where an if statement has an additional if statement indented below the first statement. This is to allow multiple choices within an if statement.
+
+print("What type of book cover does the book have?")
+cover = input()
+print()
+if (cover =="soft"):
+  print("Is the cover perfect bound? (Yes/No)")
+  answer = input() 
+  if (answer == "yes"):
+    print("Soft cover, perfect bound books are very popular!")
+  else:
+    print("Soft covers with coils or stitches are great for short books.")
+else:
+  print("Books with hard covers can be more expensive!")
+
+
+  
