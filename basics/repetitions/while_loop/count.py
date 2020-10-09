43f549e461abcd5d2201474c1b5952116fa4157a
repl.it user <replituser@example.com>
2020-10-09 12:57:06 +0000

    Added code to demonstrate the use of a while loop with a counter.

---
+++ basics/repetitions/while_loop/count.py
@@ -0,0 +1,19 @@
+#Program to tell Beep to avoid the amount of live wires.
+
+#The use of an end parameter, the print function will go onto the next line down after it has been read. The end="" function overides this and will read the next string on the next line down into the empty string("") after the function is stated. 
+
+print("""
+How many live cables must I avoid?
+""")
+live_cables = int(input())
+avoid = 0
+
+print()
+while (avoid < live_cables):
+  print ("Avoiding...", end="")
+  print ("Done! {} live cables avoided.".format (avoid))
+  avoid = avoid +1
+
+
+print("""
+All live cables have been avoided.""")
