1afafe5b574d1ccc012a87c2f216ee4b23351b16
repl.it user <replituser@example.com>
2020-10-09 21:02:24 +0000

    Added code to demonstrate the counting down with a for loop.

---
+++ basics/repetitions/for_loop/count_down.py
@@ -0,0 +1,10 @@
+#For loop to show a count down
+print("How far away are we from the cave?")
+distance =int(input())
+
+#The "distance" variable is the integer input from the user. "0 , -1" means that the sequence of the range will start at 0 from the value of the "distance" and then the "-1" will mean it will decend in values of ones. 
+for count in range (distance, 0 , -1):
+  print(count, "steps remaining.")
+
+print()
+print("We have reached the cove!")
