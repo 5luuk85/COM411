33e7f6014f976c28d7ab0cfa6cafdd0ffe1c87a4
repl.it user <replituser@example.com>
2020-10-09 21:52:06 +0000

    Added code to demonstrate the use of for loops in string indexing.

---
+++ basics/repetitions/for_loop/range.py
@@ -0,0 +1,14 @@
+print("What level of brightness is required?")
+brightness = int(input())
+print()
+print("Adjusting brightness...")
+print()
+
+#The "level" variable is local to this for statement only. 
+#Within the range (1, brightness + 1), it is stating that the start of the sequence at 1, the range is between any number between 1 and the user inputs (brightness) value and the sequence will go up in increments of 1s with the "+ 1".
+for level in range (1, brightness + 1):
+  print("Beep's brightness level:", "*" * level)
+  print("Bop's bright level:", "*" * level)
+  print()
+
+print("Adjustments complete!")
