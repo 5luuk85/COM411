15846b768103640d7f7d6ab9c50ff987f77b1efc
repl.it user <replituser@example.com>
2020-10-10 20:06:39 +0000

    Added code to demonstrate the use of nested loops and decisions.

--- basics/repetitions/for_loop/nesting.py
+++ basics/repetitions/for_loop/nesting.py
@@ -12,6 +12,7 @@ marker2_pos = -1
 
 for character in range (0, len(sequence), 1):
   pos = sequence[character] #[character] is the local variable in for statement above.
+  #Added a variable for our nest statement we need to create below.
 
   if (pos == marker):
     if (marker1_pos == -1):
@@ -22,3 +23,4 @@ for character in range (0, len(sequence), 1):
   
 
 print("The distance between the markers is", marker2_pos - marker1_pos - 1)
+#The amoujnt of positions of "marker2_pos" and "marker1_pos " will be subtracted, this will give us how many spaces it will take to get from one marker to the other. But we need to know the number of positions BETWEEN them, so to rule out the count of the last marker in the process, we can simply "-1" of one of the counted positions.
