a84ba0bde4bddb88b73acc1873ceeb41f40a962c
repl.it user <replituser@example.com>
2020-10-16 12:49:52 +0000

    Added code to demonstrate the use of return values.

---
+++ basics/functions/multiple_functions.py
@@ -0,0 +1,20 @@
+#Beep is busy reading his book on the broken bridge ladder while Bop is tring to get up the ladder and carry Beep at the same time. 
+
+#The program should use multiple functions to display a ladder.
+
+def display_ladder(steps):
+  print("|   |")
+
+  for step in range (steps):
+    print ("*****")
+    print ("|   |")
+
+
+def create_ladder():
+  print("How many steps remain?")
+  steps = int(input())
+  display_ladder(steps)
+#Notice how the variable we created "steps" is also the same name as the parameter we have set for our deflined function "display_ladder(steps)"
+#The second function will call the first function for it's use. 
+
+create_ladder()
