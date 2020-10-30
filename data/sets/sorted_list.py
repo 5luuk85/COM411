cded398c975bebc5a782bff7aee08ac6e2526d32
repl.it user <replituser@example.com>
2020-10-30 17:52:04 +0000

    Added code to demonstrate how to manipulate and sort a set.

--- data/sets/sorted_list.py
+++ data/sets/sorted_list.py
@@ -8,7 +8,8 @@ def observed():
     print("Please enter an observation:")
     user_input = input()
     observations.append(user_input)
-  
+    print()
+ 
   return observations
 
 def remove_observation(list_of_observations): 
@@ -20,6 +21,7 @@ def remove_observation(list_of_observations):
 
     print("Do you wish to remove an observation (yes/no)")
     decision = input()
+    print()
 
     if decision == "yes":
       print("Please enter the observation you wish to remove:")
@@ -27,6 +29,7 @@ def remove_observation(list_of_observations):
 
       list_of_observations.remove(remove) #(1)
       print("Done!")
+      print()
     
     else:
       loop_is_running = False #(2)
@@ -40,19 +43,25 @@ def run():
   observations = observed() #Call first function
   remove_observation(observations) #Call seconond function with parameter set to data from first funtion
 
-  observations_sets = set()
+  observations_sets = set() #Created an empty set to be populated
 
-  for observation in observations:
-    occurances = observations.count(observation)
-    observations_sets.add((observation, occurances))
+  for observation in observations: #for loop with control variable and the list "observations"
+    occurances = observations.count(observation) #(3)
+    observations_sets.add((observation, occurances)) #(4)
 
+  #(3) The "occurances" variable will count the amount times the "observation" control variable within the for loop is within the list "(observation)", in other words it counts the amount of entries in the list. 
+  #(4) The tupple of the entries "observation" and the count of entries "occurances" is added to the set "observations_sets" using the ".add" function. The tuples added must be in double round brackets in order for it to work.nonlocal
 
-
-
-  for count in sorted(observations_sets):
-    observation = count [0]
-    times_observed = count [1]
-
+  for observation, times_observed in sorted(observations_sets):#(5a, 5b)
     print(f"{observation} observed {times_observed} times.")
 
+  #(5a) The "sorted" function will often sort the given data, in this case the "(observations_sets), into an order such as alphabetical. 
+  
+  #(5b) The short-hand way to write out the for loop for an "f" or format string. It defualt picks out the key and value of the tuples within the set. The key in this case is the "observation" variable and the value is the "times_observed" variable within the for loop. When using this method, just remember that the first variable is always the key and the second is the value. Tuple structure --> (Key, Value)
+
 run()
+
+  #for count in sorted(observations_sets):
+    #observation = count [0]
+    #times_observed = count [1]
+  #The way to display the key and value if they are defined by variables, but this was is often longer to write out.
