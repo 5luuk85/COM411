a93dd8bb201c552cc7a2bdead81d863a778411b0
repl.it user <replituser@example.com>
2020-10-30 16:14:16 +0000

    Changed display outputs using for loop for neater display and added additional notes.

--- data/sets/set_from_list.py
+++ data/sets/set_from_list.py
@@ -3,8 +3,8 @@ def observe():
 
   observations = [] #Empty list
 
-  for user_input in range(3):
-    print("Please enter an observartion:")
+  for count in range(3):
+    print("Please enter an observation:")
     user_input = input()
     observations.append(user_input)
     #".append" is readinng in the user input and saving it to the variable 
@@ -18,14 +18,30 @@ def run():
   observations = observe() #The list of user inputs called into a variable
   observation_set = set() #A set will need to created 
 
+  #Populate set
   for observation in observations:
     occurances = observations.count(observation) #(1)
     observation_set.add((observation, occurances)) #(2)
 
-  #(1) The ".count" will count the occurances of an entry in the list from "observations"
+  #(1) The ".count" will count how many times the "observation" variable is within in the list "observations". 
   #(2) The ".add" is used to add elements into the set, in this case we've added a tuple. The tuple added contains the loop variable "observation" and the count of list "occurances". 
 
-  print(observation_set)
-    
- 
+  #Display set
+  for objects in observation_set: #(3)
+    observation = objects[0] #(4)
+    count = objects [1] #(4)
+
+    print(f"{observation} oberved {count} times.") #(5)
+
+    #(3) The "objects" variable is the tuples within the data set "observation_set" 
+
+    #(4) The data within the tuples will need to be assigned to a variable in order for the data to be displayed in the later print statement. "Observation" is assigned to the tuple "objects" with the data position of 1 "[0]" and "count" with the tuple "objects" position of 2 "[1]". 
+
+    #(5) "f" string, a format string where we can add the defined variables into the curly brackets
+
+
 run()
+
+  #for key, value in observation_set:
+    #print(f"{key} observed {value} times.")
+  #Above is another neater way to display the outcome, instead of assigning variables to the tuples in the list within the set, the "key, value" variables within the for loop simply defualt extracts the values from the tuple.
