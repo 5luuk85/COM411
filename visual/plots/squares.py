353d67ad1bd6e92bf44aa1d3e45e20e8f9b8ce3d
repl.it user <replituser@example.com>
2020-11-10 11:25:53 +0000

    Added code to display multiple plots.

---
+++ visual/plots/squares.py
@@ -0,0 +1,32 @@
+#The plot function can also display different outputs for the graph such as plotting the points as dots or squares for example. The line can also be changed to a dashed, dotted or solid line.
+
+import matplotlib.pyplot as plt #imports plotting library for python 
+
+def small():
+  x = [3,4,4,3,3]
+  y = [3,3,4,4,3]
+
+  plt.plot(x, y,'ro:') 
+#when using different colour and markers, the format is colour, marker shape, line type
+#In the example above the format 'ro:' for the plot is red, circle marker, dotted line 
+
+def medium():
+  x = [2,5,5,2,2]
+  y = [2,2,5,5,2]
+
+  plt.plot(x, y, 'gs--') #'gs--' green, square marker, dashed line
+
+def large():
+  x = [1,6,6,1,1]
+  y = [1,1,6,6,1]
+
+  plt.plot(x, y, 'bp-') #'bp-' blue, pentagon marker, solid line
+
+def run():
+  small()
+  medium()
+  large()
+
+  plt.show() #Remember to place brackets on the .show function
+
+run()
