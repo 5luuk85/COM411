9c345a0190c85bd97af9550c09086816e9c34400
repl.it user <replituser@example.com>
2020-11-25 16:40:06 +0000

    Added code to show a single marker in each frame of an animation.

--- visual/animations/marker.py
+++ visual/animations/marker.py
@@ -13,11 +13,11 @@ def animate(frame): #[4]
   ax.set_ylim(0, 10)
   ax.plot(frame, frame, 'ro') #[4]
 
-#[2]".cla" means clear axes, each new frame will be drawn on a new axes so that when the  plot is bening animated or drawn, they do not overlap. In this case each dot will be drawn on the different points on the frame it is on and the previous frame/drawing is not shown while the animation is running.
+#[2]".cla" means clear axes, each new frame will be drawn on a new axes so that when the  plot is bening animated or drawn, they do not overlap. In this case each marker will be drawn on the different points on the frame it is on and the previous frame/drawing is not shown while the animation is running.
 
 #[3] Sets the limits of the axis, if not set the display of the axis will change from frame to frame during the animation, which is not ideal when trying to present data.
 
-#[4] Notice how the the x and y axis are defined as the frame parameter in the function. The values of the x & y axis will be the same value of the frame at the time, i.e. if the animation is currently on frame 2, the values of the x and y axis will be 2. The frames number of frames are defined in the run function below, the values of the x and y axis in the "ax.plot" function will draw out a diaginal line due to the x and y values.
+#[4] Notice how the the x and y axis are defined as the frame parameter in the function. The values of the x & y axis will be the same value of the frame at the time, i.e. if the animation is currently on frame 2, the values of the x and y axis will be 2. The number of frames to be run through in the animation are defined in the run function below, the values of the x and y axis in the "ax.plot" function will draw out a diaginal line due to the x and y values.
 
 def run():
   global fig
