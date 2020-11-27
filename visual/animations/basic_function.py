17cce66eeac9682b74031b2125053e8698600b62
repl.it user <replituser@example.com>
2020-11-27 14:03:06 +0000

    Added code to create a basic animation function.

--- visual/animations/basic_function.py
+++ visual/animations/basic_function.py
@@ -4,17 +4,14 @@ import matplotlib.animation as animation
 fig, ax = plt.subplots()
     
 def animate(frame): 
-  print(int(frame))
+  print(frame) #Prints the frame number in the terminal 
      
 def run():
   global fig  
 
-  animation.FuncAnimation(fig, animate, frames = 10, interval = 10000)
+  simple_animation = animation.FuncAnimation(fig, animate, frames = 10, interval = 1000)
 
   plt.show()
-
-#The function should create an animation using FuncAnimation which calls the function animate, has 10 frames and an interval of 1000 milliseconds.
-#Finally, the function should show the plot.
 
       
 run()  
