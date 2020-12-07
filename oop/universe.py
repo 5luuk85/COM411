63c4571f94d941599ee1b6115200b922c73187f7
repl.it user <replituser@example.com>
2020-12-07 18:49:01 +0000

    Added to class universe.

---
+++ oop/universe.py
@@ -0,0 +1,39 @@
+import random as rnd #Importing random module
+
+from human_instance_methods import Human #Importing classes
+from robot_instance_methods import Robot
+from planet import Planet
+
+class Universe:
+
+  def __init__(self):
+    self.planets = []
+
+  def __repr__(self):
+    return f"universe(planets={self.planets})"
+
+  def __str__(self):
+    return f"The Universe contains {len(self.planets)} planets."
+
+
+  def generate(self):
+    #Creates a new planet
+    planet = Planet()
+
+    for index in range(rnd.randint(1, 10)):
+      robot = Robot(f"Robot{index}")
+      planet.add_robot(robot) 
+
+    for index in range(rnd.randint(1, 10)):
+      human = Human(f"Robot{index}")
+      planet.add_human(human) 
+
+    #Populates the list of planets
+    self.planets.append(planet)
+   
+
+
+if (__name__ == "__main__"):
+  universe = Universe()
+  universe.generate()
+  print(universe)
